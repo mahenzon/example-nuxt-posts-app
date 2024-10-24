0d466b2686cd5b8f6550bed0b8a7acea96c4b90f
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:01:17 +0300

    create GenericReaction button component

---
+++ posts-mini-app/components/button/GenericReaction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span :class="props.icon"></span>
+    <span class="title">{{ props.title }}</span>
+    <span class="count">{{ props.count }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  icon: string
+  title: string
+  count: number
+}
+const props = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
