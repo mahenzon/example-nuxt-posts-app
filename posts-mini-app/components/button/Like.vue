a253f25b20fdcfe4d3087c1515359fe680ea514d
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:02:34 +0300

    create Like button component

---
+++ posts-mini-app/components/button/Like.vue
@@ -0,0 +1,17 @@
+<template>
+  <ButtonGenericReaction
+    icon="icon-like"
+    title="Like"
+    :count="count"
+  />
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  count: number
+}
+
+const { count } = defineProps<Props>()
+</script>
+
+<style></style>
