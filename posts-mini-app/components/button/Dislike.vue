271b5acdad25f9c70387b75bbe2641715b189ba2
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:02:41 +0300

    create Dislike button component

---
+++ posts-mini-app/components/button/Dislike.vue
@@ -0,0 +1,19 @@
+<template>
+  <ButtonGenericReaction
+    icon="icon-dislike"
+    title="Trash"
+    :count="count"
+  />
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  count: number
+}
+
+const { count } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
