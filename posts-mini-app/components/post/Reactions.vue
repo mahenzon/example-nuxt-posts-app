bbe28037a3e0e7e4a36ffef4ce4c28a6f4227366
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:13:38 +0300

    create post Reactions component

---
+++ posts-mini-app/components/post/Reactions.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ButtonLike :count="reactions.likes"/>
+    <ButtonDislike :count="reactions.dislikes"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  reactions: Reactions
+}
+const { reactions } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
