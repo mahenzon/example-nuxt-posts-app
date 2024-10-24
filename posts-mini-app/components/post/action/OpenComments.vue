1b972da7a0c20e38dbda180aa509e860efbaedde
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:14:15 +0300

    create post action OpenComments component

---
+++ posts-mini-app/components/post/action/OpenComments.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtLink :to="`/posts/${id}`">
+      Open comments
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  id: number
+}
+const { id } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
