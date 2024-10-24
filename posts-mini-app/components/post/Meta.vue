c67d714a244e4d98b9f27249ae220db22ced5525
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:15:19 +0300

    create post Meta component

---
+++ posts-mini-app/components/post/Meta.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PostActionOpenComments :id="post.id" />
+    <CommonDate/>
+    <TagArray :tags="post.tags" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  post: Post,
+}
+
+const { post } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
