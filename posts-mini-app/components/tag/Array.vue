3f2c5a235ebcb04382708169cfd4171ab945cfc0
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:11:30 +0300

    create Tag Array component

---
+++ posts-mini-app/components/tag/Array.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TagDetails
+      v-for="tag in tags"
+      :name="tag"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  tags: string[]
+}
+
+const { tags } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
