399d1c46178592cd1fd9693bba77bf39bd3e57a8
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 18:31:37 +0300

    add Post Details by id page

---
+++ posts-mini-app/pages/posts/[id].vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <PostDetails v-if="post" :post="post"/>
+    <div v-else-if="loading">Loading...</div>
+    <div v-else>Post <code>{{ id }}</code> not found</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { id } = useRoute().params
+const store = usePostsStore()
+
+const loading = ref(true)
+
+const intId = Number(id)
+const post = computed((): Post | undefined => store.getPost(intId))
+
+if (Number.isInteger(intId)) {
+  await useAsyncData('get-post', () => store.loadPost(intId).then(() => true), {
+    lazy: true,
+  })
+} else {
+  // any error needed?
+  console.log('invalid post id', id)
+}
+loading.value = false
+</script>
+
+<style>
+
+</style>
