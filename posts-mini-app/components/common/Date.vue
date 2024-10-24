31265ef66e260b4907ab0ee761aba7ad18599bab
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:00:47 +0300

    create common Date component

---
+++ posts-mini-app/components/common/Date.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span class="date">{{ date }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  date?: string
+}
+
+const { date = 'Today' } = defineProps<Props>()
+
+</script>
+
+<style>
+
+</style>
