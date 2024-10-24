c8f4e79ab67c2d5ca1f26a3dcd1b829193d422c5
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 14:03:00 +0300

    create default layout

---
+++ posts-mini-app/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+
+   <div>
+    <nav>
+
+    </nav>
+
+    <slot />
+    <footer>
+      Footer
+    </footer>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
