493d0eff71fec2f1578f0002468d951ba16a5817
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:09:08 +0300

    create Tag Details component

---
+++ posts-mini-app/components/tag/Details.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span class="tag-name">{{ name }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  name: string
+}
+
+const { name } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
