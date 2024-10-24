d64a4016149cb3cde6f6ac2f060393af49848ce0
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-24 16:15:55 +0300

    create post Footer component

---
+++ posts-mini-app/components/post/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="actions">
+    <PostReactions :reactions="post.reactions" />
+    <PostMeta :post="post" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  post: Post,
+}
+
+const { post } = defineProps<Props>()
+</script>
+
+<style></style>
