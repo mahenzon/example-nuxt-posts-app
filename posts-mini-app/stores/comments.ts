3313b8695d962a52c672817f4d35aa5fd7686da2
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-25 19:17:37 +0300

    update comments store

--- posts-mini-app/stores/comments.ts
+++ posts-mini-app/stores/comments.ts
@@ -15,6 +15,14 @@ export const useCommentsStore = defineStore('commentsStore', () => {
   // state
   const data = ref<PostCommentsData>(new Map()) as Ref<PostCommentsData>
 
+  // getters
+  const getPostComments = computed(() => (postId: number): Comment[] => {
+    if (!data.value.has(postId)) {
+      return []
+    }
+    return [...data.value.get(postId)!.values()]
+  })
+
   // utils
   async function fetchComments(postId: number): Promise<Comment[]> {
     const data: { comments: Comment[] } = await $fetch(config.api.comments.getUrl(postId))
@@ -28,28 +36,25 @@ export const useCommentsStore = defineStore('commentsStore', () => {
     return data.value.get(postId)!
   }
 
-  function getPostComments(postId: number): Comment[] {
-    return [...getPostCommentsData(postId).values()]
-  }
-
   function saveComment(postId: number, comment: Comment) {
     const comments = getPostCommentsData(postId)
     comments.set(comment.id, comment)
   }
 
-  function saveItems(postId: number, comments: Comment[]) {
+  function saveComments(postId: number, comments: Comment[]) {
     comments.map(comment => saveComment(postId, comment))
   }
 
   async function loadComments(postId: number): Promise<void> {
     store.isLoading.value = true
     const comments = await fetchComments(postId)
-    saveItems(postId, comments)
+    saveComments(postId, comments)
     store.isLoading.value = false
   }
 
   return {
-    ...store,
+    isLoading: store.isLoading,
+    data,
     // actions
     getPostComments,
     loadComments,
