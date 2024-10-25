5aa8b0815707c142c88001a0b556261213429588
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-25 13:48:48 +0300

    create post details skeleton

---
+++ posts-mini-app/components/post/Skeleton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="py-4">
+    <div class="animate-pulse flex">
+      <div class="flex-1 space-y-6 py-1">
+        <div class="h-8 bg-slate-200 rounded" />
+        <div class="">
+          <div class="h-4 bg-slate-200 rounded my-3" />
+          <div class="h-4 bg-slate-200 rounded my-3" />
+          <div class="h-4 bg-slate-200 rounded my-3" />
+        </div>
+        <div class="space-y-3">
+          <div class="grid grid-cols-3 gap-4 items-center justify-center">
+            <div class="h-5 bg-slate-200 rounded-l-full rounded-r-full col-span-1" />
+            <div class="h-3 bg-slate-200 rounded col-span-2" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
