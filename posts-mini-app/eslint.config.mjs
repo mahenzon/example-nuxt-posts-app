0934307931f03b4ae6d85bd7ad52f2f3eea2c01c
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-25 08:46:59 +0300

    add eslint rule vue/max-attributes-per-line

--- posts-mini-app/eslint.config.mjs
+++ posts-mini-app/eslint.config.mjs
@@ -1,6 +1,15 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 2,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+  },
+})
