a72808e88e00d426927098fab1f6c252a175dbaa
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-26 10:15:28 +0300

    create Avatar component

---
+++ posts-mini-app/components/comment/Avatar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <span class="relative group">
+      <span
+        :class="[
+          'relative',
+          'inline-flex',
+          'items-center',
+          'justify-center',
+          'w-10',
+          'h-10',
+          'overflow-hidden',
+          'bg-white',
+          'rounded-full',
+          'ring',
+          'ring-black',
+        ]"
+      >
+        {{ initials }}
+      </span>
+      <span
+        :class="[
+          'absolute',
+          'shadow-lg',
+          'hidden',
+          'group-hover:block',
+          'bg-gray-900',
+          'text-white',
+          'font-light',
+          'px-3',
+          'py-1.5',
+          'text-sm',
+          'right-0',
+          'left-0',
+          'mx-auto',
+          'w-max',
+          '-top-1',
+          'left-14',
+          'rounded-lg',
+          'before:w-5',
+          'before:h-5',
+          'before:rotate-45',
+          'before:bg-gray-900',
+          'before:absolute',
+          'before:z-[-1]',
+          'before:top-1.5',
+          // 'before:-bottom-1',
+          'before:-left-1.5',
+          // 'before:right-0',
+          'before:mx-auto',
+        ]"
+      >
+        {{ user.fullName }}
+      </span>
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  user: UserMinimalProfile
+}
+
+const { user } = defineProps<Props>()
+
+const initials = computed(() => getInitials(user.fullName))
+</script>
+
+<style>
+
+</style>
