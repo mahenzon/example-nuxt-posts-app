5deb644d309076912d4e031a34d80846fc27dc84
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-26 10:15:54 +0300

    create Comment Details component

---
+++ posts-mini-app/components/comment/Details.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-4">
+    <CommentAvatar :user="comment.user" />
+    <CommentAuthorName :user="comment.user" />
+    <CommentText :comment="comment" />
+    <CommonDate />
+    <!-- todo action -->
+    <a class="text-red-500 hover:text-red-700 border-b border-b-red-500">
+      Delete
+    </a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  comment: Comment
+}
+
+const { comment } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
