98ad03145de92195fb054e6c9ac06d7ef98e587b
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-26 10:16:02 +0300

    create Comment List component

---
+++ posts-mini-app/components/comment/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div
+      v-for="comment in comments"
+      :key="comment.id"
+    >
+      <CommentDetails :comment="comment" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  comments: Comment[]
+}
+
+const { comments } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
