59f7e0209ff58677234419b104eca83b35b6c29f
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-26 10:15:21 +0300

    create AuthorName component

---
+++ posts-mini-app/components/comment/AuthorName.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="font-semibold">
+    {{ user.fullName }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  user: UserMinimalProfile
+}
+
+const { user } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
