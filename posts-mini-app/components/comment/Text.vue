a760dd24cb52aa9042c49dbbf7bae2388c5c1a58
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-26 10:15:46 +0300

    create Comment Text component

---
+++ posts-mini-app/components/comment/Text.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ comment.body }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  comment: Comment
+}
+
+const { comment } = defineProps<Props>()
+</script>
+
+<style>
+
+</style>
