746e06d4369d66794f52f840818db9aecdf7f432
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-26 10:16:12 +0300

    create Comments Section component

---
+++ posts-mini-app/components/comment/Section.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2 class="text-3xl font-bold">
+      {{ comments.length }} comment{{ pluralSuffix }}
+    </h2>
+    <CommentList :comments="comments" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  comments: Comment[]
+}
+
+const { comments } = defineProps<Props>()
+const pluralSuffix = computed(() => comments.length === 1 ? '' : 's')
+</script>
+
+<style>
+
+</style>
