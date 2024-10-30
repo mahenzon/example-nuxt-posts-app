badb0cd454ded901a1ba955bfdef99377ecca64d
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-30 16:44:29 +0300

    update styles for the comment skeleton for mobile

--- posts-mini-app/components/comment/SectionSkeleton.vue
+++ posts-mini-app/components/comment/SectionSkeleton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="animate-pulse">
-    <div class="h-8 bg-slate-200 rounded-md w-1/4" />
+    <div class="h-8 bg-slate-200 rounded-md w-1/2 md:w-1/4 lg:w-1/5" />
     <CommentSkeleton class="mt-4" />
   </div>
 </template>
